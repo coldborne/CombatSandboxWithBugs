31b654d002adf086235610899dc18fec8e1f61ad
coldborne <coldborneVV@gmail.com>
2026-04-23 20:41:01 +0300

    Добавлен новый компонент для управления всей игрой (На данный момент времени просто стартовая настройка)

---
+++ Assets/Scripts/Core/Game.cs
@@ -0,0 +1,25 @@
+﻿using Character;
+using UnityEngine;
+using Vehicles;
+
+namespace Core
+{
+    public class Game : MonoBehaviour
+    {
+        [SerializeField] private Player _player;
+        [SerializeField] private Vehicle _vehicle;
+
+        private Logger _logger;
+
+        private void Awake()
+        {
+            _logger = new Logger();
+        }
+
+        private void Start()
+        {
+            _player.Initialize(_logger);
+            _vehicle.Initialize(_logger);
+        }
+    }
+}
